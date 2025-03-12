470df4ed19b73ef9d5ad1ebfc2f1fd0e8da19e61
SharanjanaRagunathan <sharanjana10@gmail.com>
2025-03-12 14:40:12 +0530

    Add files via upload

---
+++ 2021ict51-day1.js
@@ -0,0 +1,95 @@
+//Q1  hello print
+		 console.log("HEllo world")
+		 
+// Q2  Arithmetic operation
+ let num1=12;
+ let num2=36;
+ 
+ 
+ let sum= num1+num2;
+ let sub= num1-num2;
+ let mult= num1*num2;
+ let div= num1/num2;
+ 
+ console.log("Sum"+sum)
+ console.log("sub"+sub)
+ console.log("Mult"+mult)
+ console.log("div"+div)
+ 
+ 
+ 
+ const z=10     
+//cant put another value for z 
+		 
+ 
+ 
+ //var 
+ var x=10
+ var x=20 
+ console.log("x: "+x)
+ 
+ 
+ var num1=12;
+ var num2=36;
+ 
+ 
+ var sum= num1+num2;
+ var sub= num1-num2;
+ var mult= num1*num2;
+ var div= num1/num2;
+ 
+ console.log("Sum"+sum)
+ console.log("sub"+sub)
+ console.log("Mult"+mult)
+ console.log("div"+div)
+ 
+ 
+ 
+ 
+ //Q3 print 1-10 numbers
+ 
+ for(var i=1;i<=10;i++)
+ {
+	 console.log(i)
+ }
+ 
+ 
+ //Q4 print odd numbers form 1-10
+ for(i=1;i<=10;i++)
+ {
+	 if(i%2==1)
+	 {
+		 console.log(i)
+	 }
+	 else{
+		 continue;
+	 }
+ }
+ 
+ 
+ //Q5 print the numbers in reverse from 10-1
+ 
+ for(let i=10;i>=1;i--)
+ {
+	 console.log(i);
+ }
+ 
+ 
+//Q6 4321 - 5 - 9876
+ var i=console.log(parseInt(9/2));
+ var s=i-1;
+ while(s>0)
+ {
+	 console.log(s);
+	 s--;
+ }
+ console.log(parseInt(9/2));
+ var j=n;
+ while(j>i)
+ {
+	 console.log(j);
+	 j--;
+ }
+ 
+ 
+  
