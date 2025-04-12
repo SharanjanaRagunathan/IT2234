20f1243f22233e75316da6c4e33ff1b748e55581
SharanjanaRagunathan <sharanjana10@gmail.com>
2025-04-12 19:41:29 +0530

    Delete Day8/Code directory

--- Day8/Code/student/studentdb.js
+++
@@ -1,9 +0,0 @@
-let students = [
-    { regno: "2021ICT01", name: "James", age: 21,gender:"male" ,course: "IT"},
-    { regno: "2021ICT02", name: "Zaara", age: 22,gender:"female" , course: "CS" },
-    { regno: "2021ICT03", name: "Michael", age: 23,gender:"male" , course: "Software Engineering" },
-    { regno: "2021ICT04", name: "Shana", age: 21, gender:"female" ,course: "IT" },
-    { regno: "2021ICT05", name: "Daniel", age: 22,gender:"male" , course: "Cybersecurity" }
-  ];
-
-  module.exports=students;
